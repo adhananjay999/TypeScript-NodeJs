84a83ca2b755b90c25a0807e3c66f5004c860970
dhananjay.adhao <dhananjay.adhao@afourtech.com>
2021-11-19 09:58:53 +0530

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+//Example 1
+var message = 'Hello World';
+console.log(message);
+// //Example 2
+// function sayHello(name: string): void {
+//   console.log('Welcome '+ name);
+// }
+// sayHello("John");
+// //Example 3
+// class Person{
+//     name: string;
+//     constructor(name: string) {
+//       this.name = name;
+//     }
+// }
+// function greeter(personName: string) {
+//     return "Hello " + personName;
+// }
+// const user = new Person("Ram");
+// console.log(greeter(user.name));
+// sayHello(user.name)
