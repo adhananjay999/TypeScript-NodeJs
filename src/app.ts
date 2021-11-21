d05d7bbe0867d250724b24d0449db030758817c8
dhananjay.adhao <dhananjay.adhao@afourtech.com>
2021-11-22 02:29:57 +0530

    example ts-types:number,boolean,string

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 const button=document.querySelector("button")!;
 const input1=document.getElementById("num1")! as HTMLInputElement;
 const input2=document.getElementById("num2")! as HTMLInputElement;
-
-function add(num1:number,num2:number){
-    return num1 + num2;
+//Example of Typescript types: number,boolean,string
+function add(num1:number,num2:number,showResult:boolean,phrse:string){
+    const result=num1+num2;
+    if(showResult){
+        console.log(phrse+result);
+    }else{
+        return result;
+    }
 }
+let number1:number;
+number1=5;
+const number2=2.8;
+const printResult=true;
+let resultPhrase='Result is: ';
 
-button.addEventListener("click", function(){
-    console.log(add(+input1.value, +input2.value));
-});
+// button.addEventListener("click", function(){
+// console.log();
+// });
+
+add(number1,number2,printResult,resultPhrase);
