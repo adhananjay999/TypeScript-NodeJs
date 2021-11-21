89ad2458abcc4aa57a0812669045c054ee9a532d
dhananjay.adhao <dhananjay.adhao@afourtech.com>
2021-11-22 04:04:46 +0530

    example union type

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,20 @@
-//Object type
-const person:object={
-name:"Dhananjay",
-age:23,
-};
-console.log(person);
-// console.log(person.name);//compile error Property 'name' does not exist on type 'object'.ts(2339)
+//union type and literal type
+enum ResultConversion {As_Number="as-number",As_Text="as-text"}
 
-
-
-//array type
-let favouriteActivities:string[];
-favouriteActivities=["Sports"];
-console.log(favouriteActivities[0]);
-
-//tuple type
-let book:[number,string];
-book=[150,"Type-Script"];
-console.log(book);
-// book[1]=10;// !!!Error Type 'number' is not assignable to type 'string'.ts(2322)
-
-//enum type
-enum Role {ADMIN,READ_ONLY,AUTHOR};
-enum Department {CSE="Computer Sciemnce",ENTC="Electronics and telecommunication", MECH="Mechanicle"};
-
-const student:{
-    name:string;
-    age:number;
-    hobbies:string[];
-    contact:[number,string];
-    role:Role;
-}={
-    name:"John",
-    age:24,
-    hobbies:["Sports","Reading"],
-    contact:[1234567890,"john@gmail.com"],
-    role:Role.ADMIN
-    };
-console.log(student.name);
-for(const hobby of student.hobbies){
-    console.log(hobby);
-    // console.log(hobby.map());//!!! Error Property 'map' does not exist on type 'string'.ts(2339)
+function combine(input1:number|string,input2:number|string,resultConversion:ResultConversion.As_Number|ResultConversion.As_Text){
+    let result;
+    // result= input1+input2;//!!!Error Operator '+' cannot be applied to types 'string | number' and 'string | number'.ts(2365)
+    if(typeof input1=="number"&&typeof input2=="number" ||resultConversion==ResultConversion.As_Number){
+        result= +input1 + +input2;
+    }
+    else{
+        result=input1.toString()+input2.toString();
+    }
+    return result;
 }
 
-if(student.role==Role.ADMIN){
-    console.log("is admin");
-}
+const combinedAges=combine(30,26,ResultConversion.As_Number);
+console.log(combinedAges);
+
+const combinedName=combine("Dhananjay ","Adhao",ResultConversion.As_Text);
+console.log(combinedName);
---
+++ src/core.ts
@@ -0,0 +1,47 @@
+//Object type
+const person:object={
+name:"Dhananjay",
+age:23,
+};
+console.log(person);
+// console.log(person.name);//compile error Property 'name' does not exist on type 'object'.ts(2339)
+
+
+
+//array type
+let favouriteActivities:string[];
+favouriteActivities=["Sports"];
+console.log(favouriteActivities[0]);
+
+//tuple type
+let book:[number,string];
+book=[150,"Type-Script"];
+console.log(book);
+// book[1]=10;// !!!Error Type 'number' is not assignable to type 'string'.ts(2322)
+
+//enum type
+enum Role {ADMIN,READ_ONLY,AUTHOR};
+enum Department {CSE="Computer Sciemnce",ENTC="Electronics and telecommunication", MECH="Mechanicle"};
+
+const student:{
+    name:string;
+    age:number;
+    hobbies:string[];
+    contact:[number,string];
+    role:Role;
+}={
+    name:"John",
+    age:24,
+    hobbies:["Sports","Reading"],
+    contact:[1234567890,"john@gmail.com"],
+    role:Role.ADMIN
+    };
+console.log(student.name);
+for(const hobby of student.hobbies){
+    console.log(hobby);
+    // console.log(hobby.map());//!!! Error Property 'map' does not exist on type 'string'.ts(2339)
+}
+
+if(student.role==Role.ADMIN){
+    console.log("is admin");
+}
