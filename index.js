976844d63402e25437d0e95eb7c8c8813ca645a2
dhananjay.adhao <dhananjay.adhao@afourtech.com>
2021-11-25 11:43:00 +0530

    wip create collection

--- index.js
+++ index.js
@@ -3,16 +3,31 @@ const app = express(); // instance of express
 const port = 3000; // Defining port 3000
 const mongoose = require("mongoose"); //instance of mongo db
 const dotenv = require("dotenv").config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 // console.log(process.env.DB_CONNECTION);
 
 //Imports Routes
 const postsRoutes = require("./routes/posts");
 
+app.use(express.json());
 //Midleware
 app.use("/posts", postsRoutes, );
+// app.use(cors());
+// // Configuring body parser middleware
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// // app.use('/posts',()=>{
+// //     console.log("this is a midleware running");
+// // });
 
-// app.use('/posts',()=>{
-//     console.log("this is a midleware running");
+app.get('/',(req,res)=>{
+res.send('We are on home');
+});
+//Connect to db
+// mongoose.connect(process.env.DB_CONNECTION, () => {
+
+//   console.log("Connected to DB");
 // });
 
 //Connect to db
--- routes/posts.js
+++ routes/posts.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
+// const mongoose = require("mongoose");
+
 
 // GET request
 router.get("/", (req, res) => {
   res.status(200).send("Server is up and running.");
+});
+router.get("/data", (req, res) => {
+  const post = Post.find();
 });
 router.get("/specific", (req, res) => {
   res.status(200).send("Specific Post");
 });
 
 // POST request
-router.post("/", (req, res) => {
-  res.status(200).send("This is a POST request");
+router.post("/",async (req, res) => {
+  try{
+    // console.log(req.body);
+  const {title,description}=req.body;
+  console.log(title + ' ' + description);
+  
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+    date: Date.now(),
+  });
+  
+  await post.save();
+  // books.push(post);
+  res.send(200).send("post is added to the database");
+  // res.status(200).send("This is a POST request");
+  }catch(err){
+      console.error('Error connecting to mongo', err);
+  };
 });
 
 // PUT Request
