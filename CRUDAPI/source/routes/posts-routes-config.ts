d4cfa18071541022ff7ab23b58e076e30c8d0ff2
dhananjay.adhao <dhananjay.adhao@afourtech.com>
2021-12-01 16:53:30 +0530

    Optimised routes:convert to class

---
+++ CRUDAPI/source/routes/posts-routes-config.ts
@@ -0,0 +1,33 @@
+// import express from 'express';
+// const router = express.Router();
+// router.get('/api/posts', controller.getPosts);
+// router.post('/api/posts', controller.addPost);
+// router.get('/api/posts/:id', controller.getPost);
+// router.put('/api/posts/:id', controller.updatePost);
+// router.delete('/api/posts/:id', controller.deletePost);
+// export = router;
+
+import { CommonRoutesConfig } from "./common/common-routes-config";
+import { Application, Request, Response, NextFunction } from "express";
+import controller from "../controllers/post";
+
+export class PostsRoutes extends CommonRoutesConfig {
+  configureRoutes(): Application {
+    // throw new Error('Method not implemented.');
+    this.app
+      .route(`/posts`)
+      .get(controller.getPosts)
+      .post(controller.addPost);
+
+    this.app
+      .route(`/posts/:postId`)
+      .get(controller.getPost)
+      .put(controller.updatePost)
+      .delete(controller.deletePost);
+
+    return this.app;
+  }
+  constructor(app: Application) {
+    super(app, "PostsRoutes");
+  }
+}
